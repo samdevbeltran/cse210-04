fed0c88c140c2daf4c192a494b66f051609c95c8
samdevbeltran <samigamedev96@gmail.com>
2022-02-22 17:25:33 -0500

    display classwas created

--- __main__.py
+++ __main__.py
@@ -8,9 +8,10 @@ from game.services.display import Display
 X_WIDTH = 700
 Y_HEIGHT = 600
 GAME_NAME = "Greed"
+FRAME_NUMBER = 12
 
 def main():
-    display = Display(X_WIDTH,Y_HEIGHT,GAME_NAME)
+    display = Display(X_WIDTH,Y_HEIGHT,GAME_NAME,FRAME_NUMBER)
     director = Director("samuel",display)
     director.start_game()
 
