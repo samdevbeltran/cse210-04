2f1f8094f9c24aaa794b9e76fdd7da8e9d99b4ff
1Vadim <2332399@gmail.com>
2022-02-24 04:21:40 +0200

    physics change

--- game/states/physics.py
+++ game/states/physics.py
@@ -1,5 +1,4 @@
 from random import randint
-import pyray
 import random
 
 from game.character.character import Character
@@ -13,26 +12,29 @@ CELL_SIZE = 15
 FONT_SIZE = 15
 
 
-class FallingGems:
+class FallingGemsRocks:
     """This will generates the falling down of the rocks and the gems """
 
     def __init__(self, video_service):
 
         self._video_service = video_service
         self._gem_position = Position()
+        self._rock_position = Position()
 
-    def create_gems(self, x, y):
-
-        cast = Cast()
+    def gem_position(self, x, y):
 
         # creates a gem
 
-        text = "*"
         x_gem = x
         y_gem = y
         position = Position(x_gem, y_gem)
         position = position.scale(CELL_SIZE)
 
+    def move_gem(self):
+        cast = Cast()
+
+        # changes the position of gem to 1 size of gem
+        text = "*"
         r = random.randint(0, 255)
         g = random.randint(0, 255)
         b = random.randint(0, 255)
@@ -42,14 +44,51 @@ class FallingGems:
         character.set_text(text)
         character.set_font_size(FONT_SIZE)
         character.set_color(color)
-        character.set_position(position)
-        cast.add_actor("character", character)
+        cast.add_character("character", character)
 
-        # draw character
-        self._video_service.draw_character(character)
-
-    def move_gem(self, ):
-        # changes the position of gem to 1 size of gem
         x = random.randint(1, COLS-1)
         y = 0
-        add(self, )
+
+        for _ in range(1, ROWS):
+            self._video_service.clear_buffer()
+            x = x
+            y = y + FONT_SIZE
+            self.gem_position(self, x, y)
+            # draw character
+            self._video_service.draw_character(character)
+
+    def rock_position(self, x, y):
+
+        # creates a gem
+
+        x_rock = x
+        y_rock = y
+        position = Position(x_rock, y_rock)
+        position = position.scale(CELL_SIZE)
+
+    def move_rock(self):
+        cast = Cast()
+
+        # changes the position of gem to 1 size of gem
+        text = "O"
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+
+        character = Character()
+        character.set_text(text)
+        character.set_font_size(FONT_SIZE)
+        character.set_color(color)
+        cast.add_character("character", character)
+
+        x = random.randint(1, COLS-1)
+        y = 0
+
+        for _ in range(1, ROWS):
+            self._video_service.clear_buffer()
+            x = x
+            y = y + FONT_SIZE
+            self.rock_position(self, x, y)
+            # draw character
+            self._video_service.draw_character(character)
