22569c1bdfc78d4a1768184254f1aa69ad1065f4
1Vadim <2332399@gmail.com>
2022-02-24 03:25:40 +0200

    physics and color changed

--- game/states/physics.py
+++ game/states/physics.py
@@ -1,87 +1,55 @@
 from random import randint
-
-
+import pyray
 import random
+
+from game.character.character import Character
+from game.states.color import Color
+from game.character.position import Position
+from game.character.cast import Cast
 
 COLS = 60
 ROWS = 40
 CELL_SIZE = 15
+FONT_SIZE = 15
 
 
-class Physics:
-    """
-    This will generates the falling down of the rocks and the gems 
-    Attributes:
-        _characters (dict): a dictionary with the characters
-        _gravity (int) : the gravity for the characters
-    Author:Vadim
-    """
+class FallingGems:
+    """This will generates the falling down of the rocks and the gems """
 
-    def __init__(self, x, y):
-        """Constructs a new point using the specified x and y values.
+    def __init__(self, video_service):
 
-        Args:
-            x (int): The specified x value.
-            y (int): The specified y value.
-        """
-        self._x = x
-        self._y = y
+        self._video_service = video_service
+        self._gem_position = Position()
 
-    def add(self, other):
-        """Gets a new point that is the sum of this and the given one.
+    def create_gems(self, x, y):
 
-        Args:
-            other (Physics): The point to add.
+        cast = Cast()
 
-        Returns:
-            Point: A new point that is the sum.
-        """
-        x = self._x + other.get_x()
-        y = self._y + other.get_y()
-        return Physics(x, y)
+        # creates a gem
 
-    def equals(self, other):
-        """Whether or not this point is equal to the given one.
-
-        Args:
-            other (Point): The point to compare.
-
-        Returns: 
-            boolean: True if both x and y are equal; false if otherwise.
-        """
-        return self._x == other.get_x() and self._y == other.get_y()
-
-    def get_x(self):
-        """Gets the horizontal distance.
-
-        Returns:
-            integer: The horizontal distance.
-        """
-        return self._x
-
-    def get_y(self):
-        """Gets the vertical distance.
-
-        Returns:
-            integer: The vertical distance.
-        """
-        return self._y
-
-    def scale(self, factor):
-        """
-        Scales the point by the provided factor.
-
-        Args:
-            factor (int): The amount to scale.
-
-        Returns:
-            Point: A new Point that is scaled.
-        """
-        return Physics(self._x * factor, self._y * factor)
-
-    def falling_gems():
         text = "*"
+        x_gem = x
+        y_gem = y
+        position = Position(x_gem, y_gem)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+
+        character = Character()
+        character.set_text(text)
+        character.set_font_size(FONT_SIZE)
+        character.set_color(color)
+        character.set_position(position)
+        cast.add_actor("character", character)
+
+        # draw character
+        self._video_service.draw_character(character)
+
+    def move_gem(self, ):
+        # changes the position of gem to 1 size of gem
         x = random.randint(1, COLS-1)
         y = 0
-        position = Point(x, y)
-        position = position.scale(CELL_SIZE)
+        add(self, )
